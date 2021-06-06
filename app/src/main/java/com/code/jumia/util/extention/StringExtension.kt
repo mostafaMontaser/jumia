d7a35f95fa7ba4696fa7c11309861fcf592af7c2
Mostafa Montaser <mostafa.montaser@ibm.com>
2021-06-06 03:40:50 +0200

    WIP

--- app/src/main/java/com/code/jumia/util/extention/StringExtension.kt
+++ app/src/main/java/com/code/jumia/util/extention/StringExtension.kt
@@ -3,8 +3,8 @@ package com.code.jumia.util.extention
 import com.code.jumia.util.parsing.ParsingHelper
 import java.lang.reflect.Type
 
-fun <T> String.convertToModel(type: Type): T {
-    return ParsingHelper.gson.fromJson<T>(this, type)
+fun <T> String.convertToModel(type: Type): T? {
+    return ParsingHelper.gson?.fromJson<T>(this, type)
 }
 
 
